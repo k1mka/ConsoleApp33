ac97c31ea06ef5c5d143685ac5043d76f597e880
k1mka <gabaidzegeorgij@gmail.com>
2022-05-07 18:49:16 +0300

    Add project files.

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
